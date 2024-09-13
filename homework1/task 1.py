7c724cbfa8965a3e8248bee421bd120385d08690
g-chakhvadze <chakhvadzeg@gmail.com>
2024-09-14 00:39:10 +0400

    homework1-task2

--- homework1/task 1.py
+++ homework1/task 1.py
@@ -3,3 +3,5 @@ name=input("თქვენი სახელია?")
 surname=input("თქვენი გვარია?") 
 age=input("თქვენი ასაკი?") 
 print("გამარჯობა " + name + " " + surname + ". თქვენი ასაკია"  + age + "წელი.")
+
+print("chasworebaaaa")
---
+++ homework1/task 2.py
@@ -0,0 +1,4 @@
+name=input("your full name?")
+birth_year=int(input("your birth year"))
+age=2024-birth_year
+print(f"your name is {name}, your age is {age} years old.")
