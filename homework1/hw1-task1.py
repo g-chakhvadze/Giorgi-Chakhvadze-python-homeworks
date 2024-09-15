60d9352e8df6b7abafdfba6c378f7e8b5a32b450
g-chakhvadze <chakhvadzeg@gmail.com>
2024-09-15 12:13:23 +0400

    Added hw1-task1.py through hw1-task4.py

---
+++ homework1/hw1-task1.py
@@ -0,0 +1,6 @@
+#task 1
+name=input("თქვენი სახელია?")
+surname=input("თქვენი გვარია?") 
+age=input("თქვენი ასაკი?") 
+print("გამარჯობა " + name + " " + surname + ". თქვენი ასაკია"  + age + "წელი.")
+
