a3d2427974b611cf379ce30dbe9e0a004460822b
g-chakhvadze <chakhvadzeg@gmail.com>
2024-09-15 13:18:27 +0400

    Remove all files from main branch

--- homework1/task 1.py
+++
@@ -1,6 +0,0 @@
-#task 1
-name=input("თქვენი სახელია?")
-surname=input("თქვენი გვარია?") 
-age=input("თქვენი ასაკი?") 
-print("გამარჯობა " + name + " " + surname + ". თქვენი ასაკია"  + age + "წელი.")
-
