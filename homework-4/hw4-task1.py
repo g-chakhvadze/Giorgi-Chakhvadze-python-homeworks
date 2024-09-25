e0a4ea41483939b098778ae6d0f752e3c8d51d75
g-chakhvadze <chakhvadzeg@gmail.com>
2024-09-25 14:17:58 +0400

    Remove homework-4 folder from homework-5 branch

--- homework-4/hw4-task1.py
+++
@@ -1,6 +0,0 @@
-import random
-players_number = int(input("Enter players number: "))
-for player in range(players_number):
-    dice1 = random.randint(1,6)
-    dice2 = random.randint(1,6)
-    print(dice1, dice2)
