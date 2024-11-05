91cbce99a159fe8fc55ec1dcd44c550cd04fed63
g-chakhvadze <chakhvadzeg@gmail.com>
2024-11-05 10:24:50 +0400

    commited task4 again

--- homework-12/hw12-task4.py
+++ homework-12/hw12-task4.py
@@ -2,7 +2,7 @@ def merge_sorted_list(list1,list2):
     merged_list = []
     i,j = 0,0
     while i<len(list1) and j<len(list2):
-        if list1[i]<=list2[j]:
+        if list1[i]<list2[j]:
             merged_list.append(list1[i])
             i+=1
         else:
